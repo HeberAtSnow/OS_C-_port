ef133e605a949f2836c339796cb44fe074eea32b
Andy-Orton <54328315+Andy-Orton@users.noreply.github.com>
2020-10-15 13:20:16 -0600

    Added cs files for deadlock and the t1c

---
+++ Deadlock/Deadlock/Deadlock.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Deadlock
+{
+    class Deadlock
+    {
+        public static object lock1 = new object();
+        public static object lock2 = new object();
+        static void Main(string[] args)
+        {
+            //create threads
+            Thread t1 = new Thread(ThreadProcOne);
+            Thread t2 = new Thread(ThreadProcTwo);
+            //start threads
+            t1.Start();
+            t2.Start();
+            //wait for threads to join
+            t1.Join();
+            t2.Join();
+            
+            Console.WriteLine("Finished");
+
+        }
+
+
+        public static void ThreadProcOne()
+        {
+            Console.WriteLine("Thread 1: Trying to Get Lock 1");
+            lock (lock1)
+            {
+                Console.WriteLine("Thread 1: Lock 1 Obtained");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1: Trying to Get Lock 2");
+                lock (lock2)
+                {
+                    Console.WriteLine("Thread 1: Done");
+                }
+            }
+        }
+
+
+        public static void ThreadProcTwo()
+        {
+            Console.WriteLine("Thread 2: Trying to Get Lock 2");
+            lock (lock2)
+            {
+                Console.WriteLine("Thread 2: Lock 2 Obtained");
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2: Trying to Get Lock 1");
+                lock (lock1)
+                {
+                    Console.WriteLine("Thread 2: Done");
+                }
+            }
+        }
+    }
+}
---
+++ t1c/t1c/t1c.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace t1c
+{
+    class t1c
+    {
+        private static object m_lock = new object();
+        private static int tally;
+        private const int MAX = 50000;
+        static void Main(string[] args)
+        {
+            //create threads
+            Thread t1 = new Thread(ThreadProc);
+            Thread t2 = new Thread(ThreadProc);
+
+            t1.Name = "Thread 1";
+            t2.Name = "Thread 2";
+
+            //start threads
+            t1.Start();
+            t2.Start();
+
+            //join threads
+            t1.Join();
+            t2.Join();
+
+            //output counter
+            Console.WriteLine("Actual Tally: {0}", tally);
+            Console.WriteLine("Expected Tally: {0}", MAX * 2);
+        }
+
+
+        public static void ThreadProc()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " has started");
+            for (int i = 0; i < MAX; i++)
+            {
+                lock(m_lock)
+                {
+                    tally++;
+                }
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " has finished");
+        }
+    }
+}
